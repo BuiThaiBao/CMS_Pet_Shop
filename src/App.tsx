d0c5a88c3ddb2bd7a0968805803b0bbd53b8b11e
buith <buithaibao2k4@gmail.com>
2025-12-18 01:04:52 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,15 @@ import SignIn from "./pages/AuthPages/SignIn";
 import SignUp from "./pages/AuthPages/SignUp";
 import NotFound from "./pages/OtherPage/NotFound";
 import UserProfiles from "./pages/User/UserProfiles";
-import Videos from "./pages/UiElements/Videos";
-import Images from "./pages/UiElements/Images";
-import Alerts from "./pages/UiElements/Alerts";
-import Badges from "./pages/UiElements/Badges";
-import Avatars from "./pages/UiElements/Avatars";
-import Buttons from "./pages/UiElements/Buttons";
+// import Videos from "./pages/UiElements/Videos";
+// import Images from "./pages/UiElements/Images";
+// import Alerts from "./pages/UiElements/Alerts";
+// import Badges from "./pages/UiElements/Badges";
+// import Avatars from "./pages/UiElements/Avatars";
+// import Buttons from "./pages/UiElements/Buttons";
 import LineChart from "./pages/Charts/LineChart";
 import BarChart from "./pages/Charts/BarChart";
 import Calendar from "./pages/Calendar";
-import BasicTables from "./pages/Tables/BasicTables";
 import FormElements from "./pages/Forms/FormElements";
 import Blank from "./pages/Blank";
 import AppLayout from "./layout/AppLayout";
@@ -45,7 +44,6 @@ export default function App() {
             }
           >
             <Route index path="/" element={<Home />} />
-
             {/* Others Page */}
             <Route path="/profile" element={<UserProfiles />} />
             <Route path="/calendar" element={<Calendar />} />
@@ -54,27 +52,26 @@ export default function App() {
             <Route path="/category/edit/:id" element={<CategoryEdit />} />
             <Route path="/product" element={<Product />} />
             <Route path="/product/add" element={<ProductAdd />} />
-            <Route path="/product/add-all-in-one" element={<ProductCreateAllInOne />} />
+            <Route
+              path="/product/add-all-in-one"
+              element={<ProductCreateAllInOne />}
+            />
             <Route path="/product/edit/:id" element={<ProductEdit />} />
             <Route path="/service" element={<Service />} />
             <Route path="/service/add" element={<ServiceAdd />} />
             <Route path="/service/edit/:id" element={<ServiceEdit />} />
             <Route path="/blank" element={<Blank />} />
-
             {/* Forms */}
             <Route path="/form-elements" element={<FormElements />} />
-
-            {/* Tables */}
+            {/* Tables
             <Route path="/basic-tables" element={<BasicTables />} />
-
             {/* Ui Elements */}
-            <Route path="/alerts" element={<Alerts />} />
+            {/* <Route path="/alerts" element={<Alerts />} />
             <Route path="/avatars" element={<Avatars />} />
             <Route path="/badge" element={<Badges />} />
             <Route path="/buttons" element={<Buttons />} />
             <Route path="/images" element={<Images />} />
-            <Route path="/videos" element={<Videos />} />
-
+            <Route path="/videos" element={<Videos />} /> */}
             {/* Charts */}
             <Route path="/line-chart" element={<LineChart />} />
             <Route path="/bar-chart" element={<BarChart />} />
